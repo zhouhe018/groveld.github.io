d5defef6e2f264913425cc71a42cc1b0fd372ba0
Travis CI <travis-ci@groveld.com>
2018-06-03 12:03:06 +0000

    Automated Build (15950206bed710e84ae7bbbb5d122921f85fe6ab)

--- sw.js
+++ sw.js
@@ -1,46 +1,93 @@
 'use strict';
 
-const cacheName = 'groveld-cache';
-const cacheFiles = [
-  'https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
-  'https://code.jquery.com/jquery-3.3.1.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
-  '/',
-  '/?utm_source=homescreen',
-  '/blog',
-  '/about',
-  '/contact',
-  '/legal/terms',
-  '/legal/privacy',
-  '/legal/cookies',
-  '/static/css/style.css',
-  '/static/js/main.js',
-  '/static/images/logo.png',
-  '/articles/htaccess-snippets',
-  '/articles/www-non-www-redirection',
-  '/articles/give-user-permission-to-edit-and-add-files-in-var-www',
-  '/articles/use-gitolite-to-control-access-to-a-git-server',
-  '/articles/open-hackerspaces-day-2014',
-  '/articles/how-to-use-gpg-to-encrypt-and-sign-messages',
-  '/articles/postfix-with-mysql-backend-and-tls',
-  '/articles/clean-urls-with-jekyll-apache',
-  '/articles/what-it-really-means-to-hack',
-  '/articles/teamspeak-3-server-on-debian-ubuntu',
-  '/articles/ohm2013-observe-hack-make'
-];
+const urlsToCache = ['/?utm_source=homescreen'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(cacheFiles);
+// Cache assets
+urlsToCache.push('/CNAME')
+urlsToCache.push('/favicon.ico')
+urlsToCache.push('/static/css/style.css')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-114x114-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-114x114.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-120x120-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-120x120.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-144x144-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-144x144.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-152x152-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-152x152.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-180x180-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-180x180.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-57x57-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-57x57.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-60x60-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-60x60.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-72x72-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-72x72.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-76x76-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-76x76.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon-precomposed.png')
+urlsToCache.push('/static/icons/apple/apple-touch-icon.png')
+urlsToCache.push('/static/icons/apple/safari-pinned-tab.svg')
+urlsToCache.push('/static/icons/icon-128x128.png')
+urlsToCache.push('/static/icons/icon-144x144.png')
+urlsToCache.push('/static/icons/icon-152x152.png')
+urlsToCache.push('/static/icons/icon-192x192.png')
+urlsToCache.push('/static/icons/icon-384x384.png')
+urlsToCache.push('/static/icons/icon-512x512.png')
+urlsToCache.push('/static/icons/icon-72x72.png')
+urlsToCache.push('/static/icons/icon-96x96.png')
+urlsToCache.push('/static/icons/windows/large.jpg')
+urlsToCache.push('/static/icons/windows/medium.jpg')
+urlsToCache.push('/static/icons/windows/small.jpg')
+urlsToCache.push('/static/icons/windows/wide.jpg')
+urlsToCache.push('/static/images/logo.png')
+urlsToCache.push('/static/js/main.js')
+
+// Cache posts
+urlsToCache.push('/articles/htaccess-snippets')
+urlsToCache.push('/articles/www-non-www-redirection')
+urlsToCache.push('/articles/give-user-permission-to-edit-and-add-files-in-var-www')
+urlsToCache.push('/articles/use-gitolite-to-control-access-to-a-git-server')
+urlsToCache.push('/articles/open-hackerspaces-day-2014')
+urlsToCache.push('/articles/how-to-use-gpg-to-encrypt-and-sign-messages')
+urlsToCache.push('/articles/postfix-with-mysql-backend-and-tls')
+urlsToCache.push('/articles/clean-urls-with-jekyll-apache')
+urlsToCache.push('/articles/what-it-really-means-to-hack')
+urlsToCache.push('/articles/teamspeak-3-server-on-debian-ubuntu')
+urlsToCache.push('/articles/ohm2013-observe-hack-make')
+
+// Cache pages
+urlsToCache.push('/about')
+urlsToCache.push('/blog')
+urlsToCache.push('/contact')
+urlsToCache.push('/legal/cookies')
+urlsToCache.push('/')
+urlsToCache.push('/legal/privacy')
+urlsToCache.push('/legal/terms')
+
+
+const cacheName = 'groveld-cache-v3';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(event) {
+  console.log('Activating Service Worker ....', event);
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
