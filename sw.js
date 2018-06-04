801588df8571f966507376b8bc496e98343e685e
Travis CI <travis-ci@groveld.com>
2018-06-04 14:00:00 +0000

    Automated Build (d30256ffcdf72467f0a99ba196d84145b3a89f56)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'groveld-1528119739';
+const CACHE_NAME = 'groveld-1528120798';
 const urlsToCache = ['/','/?utm_source=homescreen','/sw.js','/manifest.json','/static/offline.html','/static/images/offline.svg','/blog','/articles/htaccess-snippets'];
 
 self.addEventListener('install', function(event) {
