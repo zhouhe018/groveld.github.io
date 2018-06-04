5aed8d023ec7c1e1c4e8c0617f3bef58d912d6ae
Travis CI <travis-ci@groveld.com>
2018-06-04 08:11:26 +0000

    Automated Build (029ffe964d37959c04d1d251f74ac58b63b4a952)

--- sw.js
+++ sw.js
@@ -1,71 +1,16 @@
 'use strict';
 
-const cacheName = `groveld-1528049588`;
-const urlsToCache = ['/?utm_source=homescreen'];
-
-// Cache assets
-urlsToCache.push('/CNAME')
-urlsToCache.push('/favicon.ico')
-urlsToCache.push('/static/css/style.css')
-urlsToCache.push('/static/icons/android-chrome-192x192.png')
-urlsToCache.push('/static/icons/android-chrome-512x512.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-114x114-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-114x114.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-120x120-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-120x120.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-144x144-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-144x144.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-152x152-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-152x152.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-180x180-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-180x180.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-57x57-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-57x57.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-60x60-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-60x60.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-72x72-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-72x72.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-76x76-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-76x76.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon-precomposed.png')
-urlsToCache.push('/static/icons/apple/apple-touch-icon.png')
-urlsToCache.push('/static/icons/apple/safari-pinned-tab.svg')
-urlsToCache.push('/static/icons/windows/large.jpg')
-urlsToCache.push('/static/icons/windows/medium.jpg')
-urlsToCache.push('/static/icons/windows/small.jpg')
-urlsToCache.push('/static/icons/windows/wide.jpg')
-urlsToCache.push('/static/images/logo.png')
-urlsToCache.push('/static/js/main.js')
-
-// Cache posts
-urlsToCache.push('/articles/htaccess-snippets')
-urlsToCache.push('/articles/www-non-www-redirection')
-urlsToCache.push('/articles/give-user-permission-to-edit-and-add-files-in-var-www')
-urlsToCache.push('/articles/use-gitolite-to-control-access-to-a-git-server')
-urlsToCache.push('/articles/open-hackerspaces-day-2014')
-urlsToCache.push('/articles/how-to-use-gpg-to-encrypt-and-sign-messages')
-urlsToCache.push('/articles/postfix-with-mysql-backend-and-tls')
-urlsToCache.push('/articles/clean-urls-with-jekyll-apache')
-urlsToCache.push('/articles/what-it-really-means-to-hack')
-urlsToCache.push('/articles/teamspeak-3-server-on-debian-ubuntu')
-urlsToCache.push('/articles/ohm2013-observe-hack-make')
-
-// Cache pages
-urlsToCache.push('/about')
-urlsToCache.push('/blog')
-urlsToCache.push('/contact')
-urlsToCache.push('/legal/cookies')
-urlsToCache.push('/')
-urlsToCache.push('/legal/privacy')
-urlsToCache.push('/legal/terms')
-
+const cacheHash = 1528099884;
+const CACHE_NAME = 'groveld-' + cacheHash;
+const urlsToCache = ['/?utm_source=homescreen','/static/images/logo.png'];
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -73,7 +18,7 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(keys) {
       return Promise.all(keys.map(function(key) {
-        if (cacheName.indexOf(key) === -1) {
+        if (CACHE_NAME.indexOf(key) === -1) {
           return caches.delete(key);
         }
       }));
@@ -83,16 +28,14 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response) {
-        let clone = response.clone();
-        caches.open(cacheName).then(function(cache) {
-          cache.put(event.request, clone);
-        });
-        return response;
-      });
-    }).catch(function() {
-      return caches.match('/static/images/logo.png');
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
