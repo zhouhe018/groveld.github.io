d127e4ae7e7c2ca6fd60d8ca087786dab274ad70
Travis CI <travis-ci@groveld.com>
2018-06-04 12:50:28 +0000

    Automated Build (b1c2dc9df040bc47fa6cd5b3b79c0e286373ab0f)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const CACHE_NAME = 'groveld-1528116134';
-const urlsToCache = ['/','/?utm_source=homescreen','/manifest.json','/sw.js','/404','/offline','https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css'];
+const CACHE_NAME = 'groveld-1528116626';
+const urlsToCache = ['/','/?utm_source=homescreen','/manifest.json','/sw.js','/offline','/404'];
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
@@ -36,7 +36,7 @@ self.addEventListener('fetch', function(event) {
         })
         .catch(function() {
           // Offline fallback image
-          if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+          if (event.request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
             return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"><title id="offline-title">Offline</title><path fill="rgba(145,145,145,0.5)" d="M0 0h400v225H0z" /><text fill="rgba(0,0,0,0.33)" font-family="Helvetica Neue,Arial,sans-serif" font-size="27" text-anchor="middle" x="200" y="113" dominant-baseline="central">offline</text></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
           }
           // If both fail, show a generic fallback:
