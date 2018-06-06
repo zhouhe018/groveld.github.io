68fb208fe27ed7d9b40159513da1d65f2f98e932
Travis CI <travis-ci@groveld.com>
2018-06-06 15:12:41 +0000

    Automated Build (f82fe8977488cae63b7e4116156270cf4ad89e4d)

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const cacheVersion = '1528240804::';
+const cacheVersion = '1528297951::';
 const urlsToCache = [
-  '/',
   '/?utm_source=homescreen',
-  '/sw.js',
-  '/manifest.json',
-  '/browserconfig.xml'
+  '/404','/about','/atom.xml','/blog','/browserconfig.xml','/contact','/legal/cookies','/','/manifest.json','/legal/privacy','/robots.txt','/sitemap.xml','/sw.js','/legal/terms',
+  '/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make',
 ];
 
 self.addEventListener('install', function(event) {
@@ -34,4 +32,16 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheVersion + 'static').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(function() {
+        return caches.match('/404')
+      });
+    })
+  );
 });
