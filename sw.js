0b34dbe758fe0762e2146fe2041684fe4282ee6f
Travis CI <travis-ci@groveld.com>
2018-08-24 21:02:54 +0000

    Automated Build (1c5e157722ef1961eba38bed16b6e623f91bdf59)

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const cacheVersion = '1530613525::';
+const cacheVersion = '1535144572::';
 const urlsToCache = [
-  '/?utm_source=homescreen',
-  '/404','/about','/atom.xml','/blog','/browserconfig.xml','/contact','/legal/cookies','/','/manifest.json','/legal/privacy','/robots.txt','/sitemap.xml','/sw.js','/legal/terms',
+  '/404','/about','/articles/','/atom.xml','/browserconfig.xml','/contact','/terms/cookies','/','/manifest.json','/terms/privacy','/robots.txt','/sitemap.xml','/sw.js','/terms/',
   '/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make',
 ];
 
