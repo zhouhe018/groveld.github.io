85e465b05aea960aa6c1093356465376b6d6c22c
Travis CI <martin@groveld.com>
2018-10-15 21:04:38 +0000

    Automated Build (399bb2fcdceee18469424e499c5aa881ff86843a)

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const cacheVersion = '66462fc2566687a7cd3e1b8681d1a7991b136d5c::';
+const cacheVersion = '399bb2fcdceee18469424e499c5aa881ff86843a::';
 const urlsToCache = [
-'/404','/66462fc2566687a7cd3e1b8681d1a7991b136d5c.css','/66462fc2566687a7cd3e1b8681d1a7991b136d5c.js','/about','/articles/','/atom.xml','/browserconfig.xml','/','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'
+'/399bb2fcdceee18469424e499c5aa881ff86843a.css','/399bb2fcdceee18469424e499c5aa881ff86843a.js','/404','/about','/articles/','/atom.xml','/browserconfig.xml','/','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'
 ];
 
 self.addEventListener('install', function(event) {
