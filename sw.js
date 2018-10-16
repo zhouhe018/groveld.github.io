cb3733123c2649747f0a05639a433fc1b63c6b37
Travis CI <martin@groveld.com>
2018-10-16 21:03:08 +0000

    Automated Build (3712b2ac6861f6e6663ed367fc1f674ed0e54f06)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const cacheVersion = 'f8794fda56cb4ed174bcb1fb0525da5cdc94e05f::';
-const urlsToCache = ['/404','/about','/articles/','/atom.xml','/browserconfig.xml','/f8794fda56cb4ed174bcb1fb0525da5cdc94e05f.css','/f8794fda56cb4ed174bcb1fb0525da5cdc94e05f.js','/','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
+const cacheVersion = '3712b2ac6861f6e6663ed367fc1f674ed0e54f06::';
+const urlsToCache = ['/css/3712b2ac6861f6e6663ed367fc1f674ed0e54f06.css','/js/3712b2ac6861f6e6663ed367fc1f674ed0e54f06.js','/404','/about','/articles/','/atom.xml','/browserconfig.xml','/','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
