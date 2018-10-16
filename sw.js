ac8ba243400292ec308475d1dd5d479888412f6a
Travis CI <martin@groveld.com>
2018-10-16 19:01:12 +0000

    Automated Build (f8794fda56cb4ed174bcb1fb0525da5cdc94e05f)

--- sw.js
+++ sw.js
@@ -1,26 +1,24 @@
 'use strict';
 
-const cacheVersion = 'ebdba4b286649ee6cfe81f0ca0adf81c7c512908::';
-const urlsToCache = [
-'/404','/about','/articles/','/atom.xml','/browserconfig.xml','/ebdba4b286649ee6cfe81f0ca0adf81c7c512908.css','/ebdba4b286649ee6cfe81f0ca0adf81c7c512908.js','/','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'
-];
+const cacheVersion = 'f8794fda56cb4ed174bcb1fb0525da5cdc94e05f::';
+const urlsToCache = ['/404','/about','/articles/','/atom.xml','/browserconfig.xml','/f8794fda56cb4ed174bcb1fb0525da5cdc94e05f.css','/f8794fda56cb4ed174bcb1fb0525da5cdc94e05f.js','/','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(cacheVersion + 'static').then(function(cache) {
+    caches.open(cacheVersion + 'static').then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
   return self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
+        cacheNames.filter(function (cacheName) {
           return cacheName.indexOf(cacheVersion) !== 0;
-        }).map(function(cacheName) {
+        }).map(function (cacheName) {
           return caches.delete(cacheName);
         })
       );
@@ -29,15 +27,15 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.open(cacheVersion + 'static').then(function(cache) {
+    caches.open(cacheVersion + 'static').then(function (cache) {
       return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
+        return response || fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
           return response;
         });
-      }).catch(function() {
+      }).catch(function () {
         return caches.match('/404')
       });
     })
