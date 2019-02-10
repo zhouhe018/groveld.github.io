6f37a6eab8f6724922f9b2f17f1306c5e0b578e4
Travis CI <martin@groveld.com>
2019-02-10 11:46:58 +0000

    Automated Build (9e91fda70eaa656e2a0fb5f096cf50efe49ae630)

--- js/main.9e91fda.js
+++ js/main.9e91fda.js
@@ -1,9 +1,10 @@
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw2.js').then(function(registration) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
     // ServiceWorker registration successful
   }).catch(function(err) {
+    // ServiceWorker registration failed
     console.log('ServiceWorker registration failed:', err);
   });
 }
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const cacheVersion = 'ac71e7f::';
-const urlsToCache = ['/404','/about','/archive','/atom.xml','/browserconfig.xml','/','/css/main.ac71e7f.css','/js/main.ac71e7f.js','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/js/vhfix.ac71e7f.js','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
+const cacheVersion = '9e91fda::';
+const urlsToCache = ['/404','/about','/archive','/atom.xml','/browserconfig.xml','/','/css/main.9e91fda.css','/js/main.9e91fda.js','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/js/vhfix.9e91fda.js','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
