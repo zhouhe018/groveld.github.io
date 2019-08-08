f3f39647e19ff04bdadf172558a48462d8eec476
Travis CI <martin@groveld.com>
2019-08-08 12:20:29 +0000

    Automated Build (d45b31069afca063b470e09b5025fb52c9577b01)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const cacheVersion = 'b1e1618::';
-const urlsToCache = ['/404','/about','/atom.xml','/browserconfig.xml','/','/css/main.b1e1618.css','/js/main.b1e1618.js','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/edgerouter-ziggo-config','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
+const cacheVersion = 'd45b310::';
+const urlsToCache = ['/404','/about','/atom.xml','/browserconfig.xml','/','/css/main.d45b310.css','/js/main.d45b310.js','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/edgerouter-ziggo-config','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', function (event) {
