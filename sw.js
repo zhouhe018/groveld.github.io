3e1a55fc88833c6177ca1bfde568f81009f228ad
Travis CI <3328493+groveld@users.noreply.github.com>
2019-12-09 08:58:53 +0000

    Automated Build (55a6357972ca7e3b3270e2fde714fc9c27eb7447)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const cacheVersion = '360d4b6::';
-const urlsToCache = ['/404','/about','/atom.xml','/browserconfig.xml','/','/css/main.360d4b6.css','/js/main.360d4b6.js','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
+const cacheVersion = '55a6357::';
+const urlsToCache = ['/404','/about','/atom.xml','/browserconfig.xml','/','/css/main.55a6357.css','/js/main.55a6357.js','/manifest.json','/privacy','/robots.txt','/sitemap.xml','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', function (event) {
