f1839e477f52c901dd9e83728c60c1ca9331af13
Travis CI <3328493+groveld@users.noreply.github.com>
2020-02-20 20:58:02 +0000

    Automated Build (51831f817768151f2d4cf8cb08d62a2d6da12c83)

--- js/main.51831f8.js
+++ js/main.51831f8.js
@@ -1,3 +1,16 @@
+function isDarkMode() {
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+}
+
+// if (isDarkMode()) {
+//   document.documentElement.setAttribute('data-theme', 'dark');
+// } else {
+//   document.documentElement.setAttribute('data-theme', 'light');
+// }
+
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const cacheVersion = '4bd5949::';
-const urlsToCache = ['/404','/about','/atom.xml','/browserconfig.xml','/feed.json','/','/css/main.4bd5949.css','/js/main.4bd5949.js','/manifest.json','/privacy','/robots.txt','/rss.xml','/sitemap.xml','/css/syntax.4bd5949.css','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
+const cacheVersion = '51831f8::';
+const urlsToCache = ['/404','/about','/atom.xml','/browserconfig.xml','/feed.json','/','/css/main.51831f8.css','/js/main.51831f8.js','/manifest.json','/privacy','/robots.txt','/rss.xml','/sitemap.xml','/css/syntax.51831f8.css','/terms','/articles/group-policy-processing','/articles/htaccess-snippets','/articles/www-non-www-redirection','/articles/give-user-permission-to-edit-and-add-files-in-var-www','/articles/use-gitolite-to-control-access-to-a-git-server','/articles/open-hackerspaces-day-2014','/articles/how-to-use-gpg-to-encrypt-and-sign-messages','/articles/postfix-with-mysql-backend-and-tls','/articles/clean-urls-with-jekyll-apache','/articles/what-it-really-means-to-hack','/articles/teamspeak-3-server-on-debian-ubuntu','/articles/ohm2013-observe-hack-make'];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', function (event) {
